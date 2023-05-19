5941dcd00da135b6d199e680245a35cfcc526acc
prajjwal <prajjwal199275@gmail.com>
2023-05-19 17:09:28 +0530

    Implemented WelcomeService.java class

--- src/main/java/io/github/prajjwal/springbootrestservices/WelcomeService.java
+++ src/main/java/io/github/prajjwal/springbootrestservices/WelcomeService.java
@@ -1,7 +1,15 @@
 package io.github.prajjwal.springbootrestservices;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WelcomeService {
+
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
+    public String retrieveWelcomeMessage() {
+        return welcomeMessage;
+    }
 }
